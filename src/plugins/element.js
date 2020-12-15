4a1ebdecf9cd0ff75ef147ed3a28cbc1111e3efc
authur <1738238581@qq.com>
2020-12-15 14:33:23 +0800

    update goodsCate 20201215

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { Button, Input, Form, FormItem, Message, Container,
   Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card,
-   Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option } from 'element-ui'
+   Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Input)
@@ -29,7 +29,7 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
-// Vue.use()
+Vue.use(Cascader)
 // Vue.use()
 // Vue.use()
 // 将弹框组件注册为全局组件，将他挂载到vue的原型对象中
