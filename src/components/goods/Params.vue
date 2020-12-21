1c06c84100de6e95cb630515abfa856f80a04f95
authur <1738238581@qq.com>
2020-12-21 21:13:06 +0800

    optimizetion project

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -254,10 +254,10 @@ export default {
       })
     },
     // 点击展开 编辑参数对话框
-    async showEditDialog (attr_id) {
+    async showEditDialog (attrId) {
       // 查询当前参数的信息
       const { data: res } = await this.$http.get(
-        `categories/${this.catId}/attributes/${attr_id}`,
+        `categories/${this.catId}/attributes/${attrId}`,
         {
           params: { attr_sel: this.activeName }
         }
@@ -291,7 +291,7 @@ export default {
       this.$refs.editParamsFormRef.resetFields()
     },
     // 删除参数事件
-    async removeParams (attr_id) {
+    async removeParams (attrId) {
       const confirmResult = await this.$confirm('此操作将永久删除该参数, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
@@ -302,7 +302,7 @@ export default {
           return this.$message.info('已取消删除')
         }
         // 删除的业务逻辑
-        const { data: res} = await this.$http.delete(`categories/${this.catId}/attributes/${attr_id}`)
+        const { data: res} = await this.$http.delete(`categories/${this.catId}/attributes/${attrId}`)
         if (res.meta.status !== 200) {
           return this.$message.error('删除参数失败！')
         }
