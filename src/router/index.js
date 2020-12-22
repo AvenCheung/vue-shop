6b92777996e67d60b539f01f38c4b5e3e84ef505
authur <1738238581@qq.com>
2020-12-22 14:31:08 +0800

    optization project

--- src/router/index.js
+++ src/router/index.js
@@ -1,83 +1,100 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/User/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import List from '../components/goods/List.vue'
-import Add from '../components/goods/Add.vue'
-import Order from '../components/order/Order.vue'
-import Report from '../components/report/Report.vue'
+// 先前的模式
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+// import Users from '../components/User/Users.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Roles.vue'
+// import Cate from '../components/goods/Cate.vue'
+// import Params from '../components/goods/Params.vue'
+// import List from '../components/goods/List.vue'
+// import Add from '../components/goods/Add.vue'
+// import Order from '../components/order/Order.vue'
+// import Report from '../components/report/Report.vue'
+//改为懒加载的模式
+const Login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
+const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
+
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/User/Users.vue')
+const Rights = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+const Roles = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+const Cate = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Cate.vue')
+const Params = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Params.vue')
+
+const List = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/List.vue')
+const Add = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/Add.vue')
+
+const Order = () => import(/* webpackChunkName: "Order_Report" */ '../components/order/Order.vue')
+const Report = () => import(/* webpackChunkName: "Order_Report" */ '../components/report/Report.vue')
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    redirect: '/login'
-    // 路由重定向，当用户访问根目录的时自动跳转到登录页面
-  },
-  {
-    path: '/login',
-    name: Login,
-    component: Login
-  },
-  {
-    path: '/home',
-    name: Home,
-    component: Home,
-    // 重定向到welcome
-    redirect: '/welcome',
-    children: [{
-      path: '/welcome',
-      name: Welcome,
-      component: Welcome
-    }, {
-      path: '/users',
-      name: Users,
-      component: Users
-    }, {
-      path: '/rights',
-      name: Rights,
-      component: Rights
-    }, {
-      path: '/roles',
-      name: Roles,
-      component: Roles
-    }, {
-      path: '/categories',
-      name: Cate,
-      component: Cate
-    }, {
-      path: '/params',
-      name: Params,
-      component: Params
-    }, {
-      path: '/goods',
-      name: List,
-      component: List
-    }, {
-      path: '/goods/add',
-      name: Add,
-      component: Add
-    }, {
-      path: '/orders',
-      name: Order,
-      component: Order
-    }, {
-      path: '/reports',
-      name: Report,
-      component: Report
-    }]
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+      // 路由重定向，当用户访问根目录的时自动跳转到登录页面
+    },
+    {
+      path: '/login',
+      name: Login,
+      component: Login
+    },
+    {
+      path: '/home',
+      name: Home,
+      component: Home,
+      // 重定向到welcome
+      redirect: '/welcome',
+      children: [{
+        path: '/welcome',
+        name: Welcome,
+        component: Welcome
+      }, {
+        path: '/users',
+        name: Users,
+        component: Users
+      }, {
+        path: '/rights',
+        name: Rights,
+        component: Rights
+      }, {
+        path: '/roles',
+        name: Roles,
+        component: Roles
+      }, {
+        path: '/categories',
+        name: Cate,
+        component: Cate
+      }, {
+        path: '/params',
+        name: Params,
+        component: Params
+      }, {
+        path: '/goods',
+        name: List,
+        component: List
+      }, {
+        path: '/goods/add',
+        name: Add,
+        component: Add
+      }, {
+        path: '/orders',
+        name: Order,
+        component: Order
+      }, {
+        path: '/reports',
+        name: Report,
+        component: Report
+      }]
+    }
+  ]
 })
 
 // 挂载路由导航守卫 beforeEach((to, from, next) => {})
